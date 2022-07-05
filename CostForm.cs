c66b875620e8688381c37e3068c3969b66dc049c
gwnBallesteros <gwenballesteros.dev@gmail.com>
2022-07-05 19:54:49 +0800

    save

---
+++ CostForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFee
+{
+    public class CostForm
+    {
+        //Fees per year level
+        private const decimal FRESHMAN_FEE = 200.00M;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,16 +3,18 @@ using static System.Console;
 
 namespace LibraryFee
 {
-    class LibraryFee
+    class LibraryFeeApp
     {
         static void Main(string[] args)
         {
             //Declaring variables
             string studentName;
+            string librarianName;
             char classif;
 
             Header();
             studentName = InputStudentName();
+            librarianName = InputNameInCharge();
             classif = InputYearLevel();
         }
 
@@ -48,10 +50,20 @@ namespace LibraryFee
             Console.WriteLine("[B] Sophomore");
             Console.WriteLine("[C] Junior");
             Console.WriteLine("[A] Senior");
+            Console.WriteLine("[D] College");
             Console.WriteLine("===================================================");
             Write("Enter the year level: ");
             yearLevel = Convert.ToChar(ReadLine());
             return yearLevel;
+        }
+
+        static public string InputNameInCharge()
+        {
+            string nameStaff;
+            Console.Write("Name of the Librarian: ");
+            nameStaff = ReadLine();
+            return nameStaff;
+
         }
     }
 
